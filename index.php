209477eaa2bf525bc3e8205666ccc618db0994cd
ihsan <muhammadihsanaffandiii@gmail.com>
2026-07-02 19:41:22 +0700

    REVISI4

--- index.php
+++ index.php
@@ -1,6 +1,15 @@
 <?php
 
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
 session_start();
+
+if(isset($_SESSION['login'])){
+
+    header("Location: dashboard.php");
+
+}else{
+
+    header("Location: auth/login.php");
+
+}
+
+exit;
