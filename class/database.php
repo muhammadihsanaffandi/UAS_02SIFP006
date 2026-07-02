cfb210afa1fe3381001039b1d9157c4f5f99f12d
ihsan <muhammadihsanaffandiii@gmail.com>
2026-07-02 20:10:04 +0700

    dbfix

--- class/database.php
+++ class/database.php
@@ -2,20 +2,28 @@
 
 class Database
 {
-    protected $host = "localhost";
-    protected $user = "root";
-    protected $dbPassword = "";
-    protected $database = "uas_perpustakaan";
+    protected $host;
+    protected $user;
+    protected $dbPassword;
+    protected $database;
+    protected $port;
 
     protected $conn;
 
     public function __construct()
     {
+        $this->host       = getenv('MYSQLHOST');
+        $this->user       = getenv('MYSQLUSER');
+        $this->dbPassword = getenv('MYSQLPASSWORD');
+        $this->database   = getenv('MYSQLDATABASE');
+        $this->port       = getenv('MYSQLPORT') ?: 3306;
+
         $this->conn = mysqli_connect(
             $this->host,
             $this->user,
             $this->dbPassword,
-            $this->database
+            $this->database,
+            $this->port
         );
 
         if (!$this->conn) {
@@ -23,5 +31,3 @@ class Database
         }
     }
 }
-
-?>
